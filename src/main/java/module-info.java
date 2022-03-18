148630d28912a791aaf56188c5f0058f001acf1e
Klemen-Z <klemenzmahar@gmail.com>
2022-03-18 09:41:38 +0100

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.ptsd.spaceinvaders {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+
+    opens com.ptsd.spaceinvaders to javafx.fxml;
+    exports com.ptsd.spaceinvaders;
+}
