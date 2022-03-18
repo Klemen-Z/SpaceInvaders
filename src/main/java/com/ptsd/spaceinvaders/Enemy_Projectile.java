a1ef893cd34b76705bf14772fc1fe82fc7ec47ad
Klemen-Z <klemenzmahar@gmail.com>
2022-03-18 09:51:42 +0100

    Added files from previous project

---
+++ src/main/java/com/ptsd/spaceinvaders/Enemy_Projectile.java
@@ -0,0 +1,8 @@
+package com.ptsd.spaceinvaders;
+
+public class Enemy_Projectile extends Projectile{
+
+    Enemy_Projectile(int speed, int damage, int x, int y, int size) {
+        super(speed, damage, x, y, size);
+    }
+}
---
+++ src/main/java/com/ptsd/spaceinvaders/Player_Projectile.java
@@ -0,0 +1,9 @@
+package com.ptsd.spaceinvaders;
+
+public class Player_Projectile extends Projectile{
+
+
+    Player_Projectile(int speed, int damage, int x, int y, int size) {
+        super(speed, damage, x, y, size);
+    }
+}
